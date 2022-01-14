a6a7ea97b3df217725a37cccff7c958d5894b314
Daniel Gonzalez <dboyg123.dg@gmail.com>
2022-01-13 19:56:18 -0500

    refocused current objective of getting data into database

--- server/src/datamodels/mongoModel.js
+++ server/src/datamodels/mongoModel.js
@@ -1,33 +1,39 @@
-const reviewsSchema = {
-  id: Number,
-  product_id: Number,
-  rating: Number,
-  date: String,
-  summary: String,
-  body: String,
-  recommend: Boolean,
-  reported: Boolean,
-  reviewer_name: String,
-  reviewer_email: String,
-  response: Boolean,
-  helpfullness: Number,
-};
+const schemas = {
+  reviewsSchema: {
+    id: Number,
+    product_id: Number,
+    rating: Number,
+    date: String,
+    summary: String,
+    body: String,
+    recommend: Boolean,
+    reported: Boolean,
+    reviewer_name: String,
+    reviewer_email: String,
+    response: Boolean,
+    helpfullness: Number,
+  },
 
-const charReviewsSchema = {
-  id: Number,
-  charateristic_id: Number,
-  review_id: Number,
-  value: Number,
-};
+  charReviewsSchema: {
+    id: Number,
+    charateristic_id: Number,
+    review_id: Number,
+    value: Number,
+  },
 
-const characteristicsSchema = {
-  id: Number,
-  product_id: Number,
-  name: String,
-};
+  characteristicsSchema: {
+    id: Number,
+    product_id: Number,
+    name: String,
+  },
 
-const reviewsPhotosSchema = {
-  id: Number,
-  review_id: Number,
-  url: String,
-};
+  reviewsPhotosSchema: {
+    id: Number,
+    review_id: Number,
+    url: String,
+  };
+}
+
+module.exports{
+  schemas,
+}
