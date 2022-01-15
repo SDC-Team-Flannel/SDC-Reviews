ac29ac42c9142497d519001599bc2cc38b7f850a
Daniel Gonzalez <dboyg123.dg@gmail.com>
2022-01-14 20:35:49 -0500

    set up most of the routes in server. Came across the put url changing

--- server/index.js
+++ server/index.js
@@ -7,33 +7,33 @@ const port = 3000;
 app.get('/reviews/', (req, res) => {
   console.log(req.query);
   route.reviewsGet(req.query);
-  res.send('received get for reviews', '200 OK');
+  res.send('received get for reviews');
 });
 
 app.get('/reviews/meta', (req, res) => {
   console.log(req.query);
   route.reviewsMetaGet(req.query);
-  res.send('received get for metaData', '200 OK');
+  res.send('received get for metaData');
 });
 
 app.post('/reviews', (req, res) => {
-  console.log(req.query);
-  route.reviewsPost(req.query);
-  res.send('received post for reviews', '201 CREATED');
+  console.log(req.body);
+  route.reviewsPost(req.body);
+  res.send('received post for reviews');
 });
 
 // TODO: turn url into req.review_id
-app.put('/reviews', (req, res) => {
+app.put('/helpful', (req, res) => {
   console.log(req.query);
   route.markHelpful(req.query);
-  res.send('received post for helpful', '204 CONTENT');
+  res.send('received post for helpful');
 });
 
 // TODO: turn url into req.review_id
-app.put('/reviews', (req, res) => {
+app.put('/report', (req, res) => {
   console.log(req.query);
   route.reviewsPost(req.query);
-  res.send('received post for reviews', '204 CONTENT');
+  res.send('received post for reviews');
 });
 
 app.listen(port, () => {
