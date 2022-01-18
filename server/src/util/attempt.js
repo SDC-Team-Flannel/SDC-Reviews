b6767e9001251f991a31c5a3515b06bdba8670c1
Daniel Gonzalez <dboyg123.dg@gmail.com>
2022-01-18 13:38:14 -0500

    work on ETL queries

--- server/src/util/attempt.js
+++ server/src/util/attempt.js
@@ -5,7 +5,14 @@ const { pool, client } = require('../middleware/postgresAPI.js');
 fs.createReadStream('../datasources/example.csv')
   .pipe(csv())
   .on('data', (row) => {
-    console.log(row);
+    client.query(`SELECT * FROM reviews_csv RETURNING id;`, [], (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('this is query result', res);
+      }
+    });
+    console.log('row parsed', typeof row);
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
