939cd4d312882ead15405fb2640bc9a1e6dec1cb
Daniel Gonzalez <dboyg123.dg@gmail.com>
2022-01-18 15:05:30 -0500

    changed approach for ETL, loaded some not all data into staging

--- server/src/util/attempt.js
+++ server/src/util/attempt.js
@@ -2,17 +2,22 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const { pool, client } = require('../middleware/postgresAPI.js');
 
-fs.createReadStream('../datasources/example.csv')
+client.connect();
+
+fs.createReadStream(fileName, tableName)
   .pipe(csv())
   .on('data', (row) => {
-    client.query(`SELECT * FROM reviews_csv RETURNING id;`, [], (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('this is query result', res);
+    client.query(
+      `INSERT INTO ${tableName} (json_info) VALUES ($1)`,
+      [row],
+      (err, res) => {
+        if (err) {
+          console.log('error in query', err);
+        } else {
+          console.log('DOWNLOAD in Progress');
+        }
       }
-    });
-    console.log('row parsed', typeof row);
+    );
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
